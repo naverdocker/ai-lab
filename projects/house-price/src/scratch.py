500179cd0a07517ec13a2fa7c8cd4845452a5a22
Naveen Chandar J <naverdocker@gmail.com>
2025-11-02 01:25:44 +0530

    house-price: scratch.py experimental day 1

--- projects/house-price/src/scratch.py
+++ projects/house-price/src/scratch.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
-import pandas as pd
 
-print ("Hello")
-print ("test")
+import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+
+PROJECT = Path(__file__).resolve().parents[1]
+DATA_PATH = PROJECT / "data" / "raw.csv"
+
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+
+df = df.rename(columns={"MedHouseVal": "rent"})
+
+DATA_PATH.parent.mkdir(exist_ok=True, parents=True)
+df.to_csv(DATA_PATH, index=False)
+
+#print(f"Saved {DATA_PATH} with shape {df.shape}")
+#print()
